21e27213c3e0bd7691eddddaee32d7c7037dd912
Derek Morey <derek.o.morey@gmail.com>
2022-02-01 23:09:21 -0500

    Added topics percent visual
    The topics page badges now show a visual indication of how many posts a
    certain topics has by giving a percentage width to each badge. This was
    done to make it easier to see which topics have more posts. The list is
    also now sorted by tags with most posts first.

--- index.js
+++ index.js
@@ -14,11 +14,40 @@ const feed = require('metalsmith-feed');
 const metafiles = require('metalsmith-metafiles');
 const tags = require('metalsmith-tags');
 
+// custom plugins
 function skip(opts) {
 	if (!opts.test()) {
 		return opts.plugin(opts.opts);
 	}
 	return (files, metalsmith, done) => {
+		setImmediate(done);
+	};
+}
+
+function tagPercents(opts) {
+	const tagsKey = opts.tagsKey || 'tags';
+	const metaKey = opts.metaKey || 'topics';
+
+	return (files, metalsmith, done) => {
+		const metadata = metalsmith.metadata();
+		const tags = metadata[tagsKey];
+
+		const tagSum = Object
+			.values(tags)
+			.reduce((acc, item) => acc + item.length, 0);
+
+		const topics = Object.entries(tags)
+			.map((entry) => {
+				const [tag, posts] = entry;
+				return {
+					name: tag,
+					slug: tag.slug,
+					percent: Math.floor((posts.length / tagSum) * 100),
+					count: posts.length,
+				}
+			});
+
+		metadata[metaKey] = topics;
 		setImmediate(done);
 	};
 }
@@ -80,6 +109,10 @@ Metalsmith(__dirname)
 	skipMetaData: false,
 	metadataKey: 'category',
 	slug: {mode: 'rfc3986'},
+}))
+.use(tagPercents({
+	tagsKey: 'category',
+	metaKey: 'topics'
 }))
 .use(inPlace({
 	engineOptions: {
